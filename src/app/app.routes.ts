fa387e079fb77c8344b4ccb196511199071c51d9
Gourav Kumar Das <gouravkumardas4@gmail.com>
2024-11-29 12:24:30 +0530

    done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'register-user', component: RegisterUserComponent }
+];
